646c8c81628f0bfea7078fc638f63a47cd640dc5
Anil Aygun <anlaygun1@gmail.com>
2026-07-27 13:00:54 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,8 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
+const builtinCommands: Array<string> = ["echo", "exit","type"];
+
 // TODO: Uncomment the code below to pass the first stage
  rl.prompt();
 
@@ -17,6 +19,15 @@ const rl = createInterface({
     else if  (command.startsWith("echo ")) {
         let message = command.slice(5);
         console.log(message);
+    }
+    else if (command.startsWith("type ")) {
+        let message = command.slice(5);
+        if (builtinCommands.includes(message)) {
+            console.log(`${message} is a shell builtin`);
+        }
+        else {
+            console.log(`${message}: not found`);
+        }
     }
     else {
         console.log(`${command}: command not found`);
